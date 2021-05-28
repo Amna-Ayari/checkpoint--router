f7cbb3da4708bb7fe9da6680a8e20b5e2af4642f
Amna-Ayari <ayariamna2018@gmail.com>
2021-05-28 23:25:48 +0200

    first commit

---
+++ src/components/ListsMovies.js
@@ -0,0 +1,28 @@
+export const ListsMovies =[{
+    id:Math.random(),
+    title:"SAW",
+    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    posterUrl:"https://i.ytimg.com/vi/ybRO7jwN4aw/maxresdefault.jpg",
+    rate:4,
+    trailler:"https://www.youtube.com/embed/N_fM-NmWA1M" 
+
+},
+{
+    id:Math.random(),
+    title:"Resident Evil",
+    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    posterUrl:"https://i1.wp.com/3.bp.blogspot.com/_X8ggyq9cwIk/TIn7Vpj9B2I/AAAAAAAAAAM/rcvELHwY26g/s1600/Resident+Evil+5+Movie.jpg?ssl=1",
+    rate:5,
+    trailler:"https://www.youtube.com/embed/HRoxrBRaYXQ" 
+
+},
+{
+    id:Math.random(),
+    title:"The Lord of the Rings",
+    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    posterUrl:"https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg",
+    rate:4,
+    trailler:"https://www.youtube.com/embed/rCZ3SN65kIs"
+
+}
+]
